ceda4f0777c969099cc0ff292563efbee13075a4
Mateo Etchepare <142445190+Matew056@users.noreply.github.com>
2025-03-21 19:43:36 -0300

    Add files via upload

---
+++ 21-03-25/js/fs.js
@@ -0,0 +1,8 @@
+
+console.log("hello world");
+const button = document.getElementById("test-button");
+function test () {
+    for (let i = 0; i<10; i++) {
+        console.log("iteration ",i)
+    }
+}
---
+++ 21-03-25/notas.md
@@ -0,0 +1,11 @@
+path relativo / absoluto
+
+path absoluto:
+disk/folder0/folder/1/folder2/file
+
+path relativo:
+/folder2/file
+
+tarea: 
+-aprender comandos
+-aprender html bien
