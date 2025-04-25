187307de40bf74bed4903cf8b0568820c9ac333a
Brian Gonzalez <briangonzaz305@gmail.com>
2025-04-25 20:46:59 -0300

    test2

--- 11-04-25/js/fn.js
+++ 11-04-25/js/fn.js
@@ -2,22 +2,26 @@ const url = "https://pokeapi.co/api/v2/pokemon";
 
 const button = document.getElementById("button")
 
+let characters = []
+
 async function getCharacter() {
     for(let i = 1; i < 2; i++) {
         const response = await fetch(`${url}/${i}`);
         character = await response.json();
         console.log(`${url}/${i}`)
         console.log(character);
+
+        `mi edad es ${variable}`
+        'mi edad es ' + variable
     }
 }
-
 // stats
 /*
 conseguir attack stats de 2 personajes y ver quien gana
  */ 
 // RETO CONSEGUIRLO EN 2 LLAMADAS A LA API
 async function getRandomCharacters () {
-    let characters = []
+    
     for (let i = 0; i < 2; i++) {
         // random number
         let randomNumber = Math.round(Math.random() * (1302 - 1) + 1)
@@ -33,13 +37,9 @@ async function getRandomCharacters () {
 
 }
 
-/*
-stats[0]=hp
-stats[1]=attack
-stats[2]=defense
-*/
 
-function generateUI (characters) {
+
+function generateUI () {
     let img1 = document.createElement('img');
     img1.src = characters[0].sprites.front_default
     img1.alt = characters[0].forms.name
@@ -61,21 +61,25 @@ function generateUI (characters) {
     console.log('UI generated')
 }
 
-//
-button.addEventListener("click", () => {
+//  
+//button.addEventListener("click", compareAttacks())
 
-})
+/*
+stats[0]=hp
+stats[1]=attack
+stats[2]=defense
+*/
 
-function compareAttacks (characters) {
-    if (characters[0].stats[2]<characters[1].stats[1]) {
-        console.log(`ataque de ${characters[0].forms.name} es mayor que la defensa de ${characters[1].forms.name}`)
-        
-    } else if (characters[0].stats[2]>characters[1].stats[1]) {
-        console.log(`ataque de ${characters[1].forms.name} es mayor a la defensa de ${characters[0].forms.name}`)
+function compareAttacks () {
+    console.log(`ataque de ${characters[0].forms[0].name} es  ${characters[0].stats[2].base_stat}`)
+    console.log(`defensa de ${characters[1].forms[0].name} es ${characters[1].stats[1].base_stat}`)
+    if (characters[0].stats[2].base_stat<characters[1].stats[1].base_stat) {
+        console.log(`la defensa de ${characters[0].forms[0].name} es menor al ataque de ${characters[1].forms[0].name}`)
+    } else if (characters[0].stats[2].base_stat>characters[1].stats[1].base_stat) {
+        console.log(`la defensa de ${characters[0].forms[0].name} es mayor al ataque de de ${characters[1].forms[0].name}`)
     } else {
         console.log(`el ataque es igual a la defensa`)
     }
-
 }
 
 
