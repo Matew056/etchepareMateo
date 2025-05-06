fb2a7d4550293d81889f8d011d415ba32ed7d0aa
Matew056 <mateodemian@gmail.com>
2025-05-06 13:11:27 -0300

    poke api branch

--- 21-03-25/js/fs.js
+++
@@ -1,8 +0,0 @@
-
-console.log("hello world");
-const button = document.getElementById("test-button");
-function test () {
-    for (let i = 0; i<10; i++) {
-        console.log("iteration ",i)
-    }
-}
